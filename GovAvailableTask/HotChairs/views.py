7f1e8daff0ce9023937301acb3cc3157b456c12a
tbenjy <benjy_t@yahoo.com>
2021-06-04 14:43:37 +0300

    Update views.py

--- GovAvailableTask/HotChairs/views.py
+++ GovAvailableTask/HotChairs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import pandas as pd
 import pymongo
+import os
 
 
 # Create your views here.
@@ -17,12 +18,12 @@ def index(request):
 
 
 def get_organizations_list(request):
-    #client = pymongo.MongoClient("mongodb+srv://govavailabletask:hotchairs@hotchairs.8vnio.mongodb.net/HotChairs?retryWrites=true&w=majority")
-    #db = client.hotchairs
-    #db = client['hotchairs']
-    #list_of_db = client.list_database_names()
+    client = pymongo.MongoClient('mongodb+srv://govavailabletask:hotchairs@hotchairs.8vnio.mongodb.net/HotChairs?retryWrites=true&w=majority')
+    #client = pymongo.MongoClient(os.environ['MONGODB_URI'])
+    db = client.get_default_database()
 
-    organization_df = pd.DataFrame({"organizationID": [1, 2], "Name": ["ארגון ראשון", "ארגון שני"]})
+    organization_df = pd.DataFrame({"organizationID": [1, 2], "Name": [db.name, "ארגון שני"]})
+    #organization_df = pd.DataFrame({"organizationID": [1, 2], "Name": ["ארגון ראשון", "ארגון שני"]})
     return JsonResponse(organization_df.to_json(orient='records'), safe=False)
 
 
