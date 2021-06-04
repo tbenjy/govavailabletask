48261a304be4f69d798927eda1d8a09ef741b4e5
tbenjy <benjy_t@yahoo.com>
2021-06-04 14:56:49 +0300

    Update views.py

--- GovAvailableTask/HotChairs/views.py
+++ GovAvailableTask/HotChairs/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import pandas as pd
 import pymongo
-import os
 
 
 # Create your views here.
@@ -21,8 +20,7 @@ def get_organizations_list(request):
     client = pymongo.MongoClient('mongodb+srv://govavailabletask:hotchairs@hotchairs.8vnio.mongodb.net/HotChairs?retryWrites=true&w=majority')
     db = client.get_default_database()
 
-    organization_df = pd.DataFrame({"organizationID": [1, 2], "Name": [db.name, db.name]})
-    #organization_df = pd.DataFrame({"organizationID": [1, 2], "Name": ["ארגון ראשון", "ארגון שני"]})
+    organization_df = pd.DataFrame({"organizationID": [1, 2], "Name": ["ארגון ראשון", "ארגון שני"]})
     return JsonResponse(organization_df.to_json(orient='records'), safe=False)
 
 
